49b27660a7ae45fa9f5b3c46b39694a81903227c
Amber Shipley <ashipley2@me.com>
2023-03-01 13:43:30 -0800

    Implement get booking functionality and update test

--- src/classes/Customer.js
+++ src/classes/Customer.js
@@ -1,11 +1,20 @@
+import Booking from './Booking';
+
 class Customer {
     constructor(customerData) {
         this.id = customerData.id;
         this.name = customerData.name;
-        this.bookings = [];
     }
-    getBookings() {
-        
+    getBookings(bookingData) {
+        const bookings = [];
+        const foundBookings = bookingData.filter(foundBooking => foundBooking.userID === this.id);
+        foundBookings.forEach(foundBooking => {
+            bookings.push(new Booking(foundBooking));
+        });
+        if (!bookings) {
+            return;
+        }
+        return bookings;
     }
 }
 
