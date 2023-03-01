8631007a33c387c2b987ce70ae125020707ee13a
Amber Shipley <ashipley2@me.com>
2023-03-01 15:58:55 -0800

    Begin implementing get requests for data use with classes

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,26 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
+import './images/sunburst.png';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+const usersRoomsSection = document.querySelector('#user-booked-section');
+const bookRoomSection = document.querySelector('#book-room-section');
 
+const fetchCustomerData = fetch('http://localhost:3001/api/v1/customers')
+    .then(response => response.json());
+const fetchBookingData = fetch('http://localhost:3001/api/v1/bookings')
+    .then(response => response.json());
+const fetchRoomData = fetch('http://localhost:3001/api/v1/rooms')
+    .then(response => response.json());
 
-console.log('This is the JavaScript entry file - your code begins here.');
+Promise.all([fetchCustomerData, fetchBookingData, fetchRoomData])
+    .then(data => {
+        console.log(data)
+        let allData = {
+            customers: data[0].customers,
+            bookings: data[1].bookings,
+            rooms: data[2].rooms
+        }
+        return allData;
+    })
+    .then(allData => {
+
+    })
