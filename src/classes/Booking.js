49999a81e5a8a49c81b23d6f5892811d54fb6719
Amber Shipley <ashipley2@me.com>
2023-03-03 18:30:12 -0800

    Format the date in a function rather than constructor

--- src/classes/Booking.js
+++ src/classes/Booking.js
@@ -1,4 +1,4 @@
-import Room from './Room';
+import { DateTime } from 'luxon';
 
 class Booking {
     constructor(bookingData, rooms) {
@@ -7,9 +7,13 @@ class Booking {
         this.date = bookingData.date;
         this.roomNumber = bookingData.roomNumber;
         this.room = this.setRoom(rooms);
+        this.dateInstance =  DateTime.fromFormat(this.date, 'yyyy/MM/dd')
     }
     setRoom(rooms) {
         return rooms.find(room => room.number === this.roomNumber);
+    }
+    formatDate() {
+        return this.dateInstance.toLocaleString(DateTime.DATE_MED);
     }
 }
 export default Booking;
