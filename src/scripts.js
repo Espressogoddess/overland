7955e59fddeabb08041efdb9c475f64a774b3553
Amber Shipley <ashipley2@me.com>
2023-03-04 18:45:41 -0800

    Implement post fetch request upon booking
    Update renderPage function to not take arg

--- src/scripts.js
+++ src/scripts.js
@@ -13,6 +13,8 @@ const dateForm = document.querySelector('#select-a-date');
 const availableRoomsSection = document.querySelector('#available-rooms');
 const availableRoomSectionTitle = document.querySelector('#room-avail-title');
 const backButton = document.querySelector('#back-button');
+const confirmationTitle = document.querySelector('#confirmation-title');
+const confirmationSection = document.querySelector('#confirmation-section');
 
 let customer;
 let hotel;
@@ -30,41 +32,66 @@ Promise.all([fetchCustomerData, fetchBookingData, fetchRoomData])
     .then(data => {
         customer = new Customer(data[0].customers[7]);
         hotel = new Hotel(data[2].rooms, data[1].bookings);
-        renderPage(customer.getBookings(hotel));
+        renderPage();
     });
 
 backButton.addEventListener('click', () => {
     currentView = 'dashboard';
-    renderPage(customer.getBookings(hotel));
+    renderPage();
 });
 
 dateForm.addEventListener('submit', (event) => {
     event.preventDefault();
     currentView = 'book';
-    renderPage(hotel.filterByDate(selectedDate));
+    renderPage();
 });
 
 availableRoomsSection.addEventListener('click', (event) => {
     if (event.target.dataset.roomNumber) {
-        const roomNumber = event.target.dataset.roomNumber;
+        const roomNumber = parseInt(event.target.dataset.roomNumber);
         currentView = 'dashboard';
         hotel.addNewBooking(hotel.getSelectedRoom(roomNumber), customer, selectedDate);
-        //if successfully booked show success page
-        //if not show error
-        setTimeout(renderPage, 4000, customer.getBookings(hotel))
+        fetch('http://localhost:3001/api/v1/bookings', {
+            method: 'POST',
+            body: JSON.stringify({ 
+                "userID": customer.id,
+                "date": selectedDate,
+                "roomNumber": roomNumber
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.message.includes('success')) {
+                    console.log(data.newBooking.id)
+                    //use ^ to look up booking and render details to the page in render fn
+                    currentView = 'confirmation';
+                    renderPage() 
+                }
+            })
+            //add success page if successful post
+            //add error page to try again if not
+        setTimeout(() => {
+            currentView = 'dashboard';
+            renderPage();
+        }, 4000)
     } else {
         return
     }
 });
 
 
-function renderPage(bookingsOrRooms) {
+function renderPage() {
     if (currentView === 'dashboard') {
         userDashboard.classList.remove('hidden');
         bookRoomSection.classList.add('hidden');
+        confirmationSection.classList.add('hidden');
+        const bookings = customer.getBookings(hotel)
         totalSpent.innerText = `$${customer.getTotalSpent(hotel)}`;
         bookedTableBody.innerHTML = '';
-        bookingsOrRooms.forEach(booking => {
+        bookings.forEach(booking => {
             bookedTableBody.innerHTML += `
             <tr>
                 <td scope="row">${booking.formatDate()}</td>
@@ -76,6 +103,7 @@ function renderPage(bookingsOrRooms) {
     } else if (currentView === 'book') {
         userDashboard.classList.add('hidden');
         bookRoomSection.classList.remove('hidden');
+        confirmationSection.classList.add('hidden');
         const availableRooms = hotel.filterByDate(selectedDate);
         const formattedDate = DateTime
             .fromFormat(selectedDate, 'yyyy/MM/dd')
@@ -107,11 +135,18 @@ function renderPage(bookingsOrRooms) {
         } else {
             availableRoomSectionTitle.innerText = `Sorry! There are not any available rooms on ${formattedDate}`;
         }
+    } else if (currentView === 'confirmation') {
+        confirmationSection.classList.remove('hidden');
+        bookRoomSection.classList.add('hidden');
+        userDashboard.classList.add('hidden');
+        confirmationTitle.innerText = 'Yeah';
     }
 }
 
 const picker = datepicker('#date-selection', {
+    minDate: new Date(), 
     onSelect: (instance, date) => {
         selectedDate = DateTime.fromJSDate(date).toISODate().split('-').join('/');
     }
 });
+picker.calendarContainer.style.setProperty('font-size', '.85rem');
