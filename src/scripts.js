44ceaef8e0f6f768a73324eed0a9c217bebc1dbd
Amber Shipley <ashipley2@me.com>
2023-03-04 16:29:29 -0800

    Populate available rooms after selecting a date

--- src/scripts.js
+++ src/scripts.js
@@ -5,15 +5,18 @@ import Hotel from './classes/Hotel';
 import { DateTime } from 'luxon';
 import datepicker from 'js-datepicker';
 
-const usersRoomsSection = document.querySelector('#user-booked-section');
+const userDashboard = document.querySelector('#user-booked-section');
 const bookRoomSection = document.querySelector('#book-room-section');
 const totalSpent = document.querySelector('#total-spent');
 const bookedTableBody = document.querySelector('#booking-tbody');
 const dateForm = document.querySelector('#select-a-date');
+const availableRoomsSection = document.querySelector('#available-rooms');
+const availableRoomSectionTitle = document.querySelector('#room-avail-title');
 
 let customer;
 let hotel;
-let currentDate;
+let selectedDate;
+let currentView = 'dashboard';
 
 const fetchCustomerData = fetch('http://localhost:3001/api/v1/customers')
     .then(response => response.json());
@@ -28,25 +31,64 @@ Promise.all([fetchCustomerData, fetchBookingData, fetchRoomData])
         hotel = new Hotel(data[2].rooms, data[1].bookings);
         const customerBookings = customer.getBookings(hotel);
         renderPage(customerBookings);
-});
+    });
 
-function renderPage(bookings) {
-    totalSpent.innerText = `$${customer.getTotalSpent(hotel)}`;
-    bookedTableBody.innerHTML = '';
-    bookings.forEach(booking => {
-        bookedTableBody.innerHTML += `
+dateForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    currentView = 'book';
+    const availableRooms = hotel.filterByDate(selectedDate);
+    renderPage(availableRooms);
+})
+
+function renderPage(bookingsOrRooms) {
+    if (currentView === 'dashboard') {
+        totalSpent.innerText = `$${customer.getTotalSpent(hotel)}`;
+        bookedTableBody.innerHTML = '';
+        bookingsOrRooms.forEach(booking => {
+            bookedTableBody.innerHTML += `
             <tr>
                 <td scope="row">${booking.formatDate()}</td>
                 <td>${booking.room.number}</td>
                 <td>${booking.room.type}</td>
                 <td>$${booking.room.costPerNight.toFixed(2)}</td>
-            </tr>`
-    });
+            </tr>`;
+        });
+    } else if (currentView === 'book') {
+        userDashboard.classList.add('hidden');
+        bookRoomSection.classList.remove('hidden');
+        const availableRooms = hotel.filterByDate(selectedDate);
+        const formattedDate = DateTime.fromFormat(selectedDate, 'yyyy/MM/dd').toLocaleString(DateTime.DATE_MED)
+        if (availableRooms.length) {
+            availableRoomSectionTitle.innerText = `Available Rooms on ${formattedDate}`;
+            availableRoomsSection.innerHTML = '';
+            availableRooms.forEach(room => {
+                availableRoomsSection.innerHTML += `
+        <div class="col-3">
+        <div class="card">
+          <div class="card-body">
+            <div class="d-flex justify-content-between mb-2">
+              <h6 class="card-title">${room.type}</h6>
+              <h6 class="card-title">Room #${room.number}</h6>
+            </div>
+            <div class="d-flex justify-content-between">
+                <p class="card-text">beds: ${room.numBeds} ${room.bedSize}</p>
+                <p class="card-text">$${room.costPerNight.toFixed(2)}</p>
+            </div>
+            <div class="d-flex justify-content-end">
+                <button class="mb-0 small-button" data-room-number="${room.number}">Book</button>
+            </div>
+          </div>
+        </div>
+        </div>`;
+            })
+        } else {
+            availableRoomSectionTitle.innerText = `Sorry! There are not any available rooms on ${formattedDate}`;
+        }
+    }
 }
 
-const picker = datepicker('#date-selection', {onSelect: (instance, date) => {
-    currentDate = DateTime.fromJSDate(date).toISODate().split('-').join('/');
-
-    console.log(hotel.filterByDate(currentDate))
-
-}});
+const picker = datepicker('#date-selection', {
+    onSelect: (instance, date) => {
+        selectedDate = DateTime.fromJSDate(date).toISODate().split('-').join('/');
+    }
+});
